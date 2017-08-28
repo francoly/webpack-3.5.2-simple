939959ba4192ad4aa1a45c7c965a272174a731c9
francoly <1219684149@qq.com>
2017-08-28 10:17:44 +0800

    li

---
+++ src/components/login/login.js
@@ -0,0 +1,36 @@
+/**
+ * Created by francoly on 2017/8/27.
+ */
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Goods from './goods.vue'
+import Goods_detail from './goods_detail.vue'
+import Goods_list from './goods_list.vue'
+import $ from 'jquery'
+Vue.use(VueRouter);
+//·������  //��Ҫ�Ӻ�׺
+var routes=[
+    //{
+    //    path:'/goods.html',
+    //    component:Goods_list
+    //},
+    //{
+    //    path:'/goods_detail',
+    //    component:Goods_detail
+    //}
+    //,
+    //{
+    //    path: '*',
+    //    component: Goods_list
+    //}
+
+];
+const router=new VueRouter({
+    scrollBehavior: () => ({ y: 0 }), // ��������������Ϊ���������Ĭ�Ͼͻ����ԭ����������λ��
+    routes
+});
+new Vue({
+    el: '#app',
+    router,
+    render: h => h(Goods)
+});
---
+++ src/components/products/products.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="app">
+        <keep-alive>
+            <router-view></router-view>
+        </keep-alive>
+    </div>
+</template>
+<script>
+    import $ from 'jquery'
+    console.log($('body'));
+    export default {
+        name: 'app',
+        data:function () {
+            return {
+                msg: 'Welcome to Your Vue.js App11'
+            }
+        }
+    }
+</script>
+
+<style lang="less">
+    @color:red;
+    body{
+        background-color:@color;
+    }
+</style>
